37f946d2f3eb23300305a389da3091e1fdfc9545
1Louu <luc.bouvard@free.fr>
2025-09-30 09:46:13 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,75 @@
+// Style prédéfini de TailwindCSS pour le bouton Retour
+const ReturnHide = "font-bold py-10 text-3xl hidden";
+const ReturnShow = "font-bold py-10 text-3xl text-white";
+
+// Style prédéfini de TailwindCSS pour le bouton Retour
+const Unselect = "bg-gray-700 flex flex-col w-lg m-5";
+const Select = "bg-gray-700 flex flex-col w-6xl m-5";
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const returnButton = document.getElementById("return");
+
+    returnButton.addEventListener('click', (e)=>{
+        HideContent(); 
+    });
+
+    
+    //Liste des blocs de contenu d'info. 
+    const bloc1 = document.getElementById("bloc1");
+    const descript1 = document.getElementById("descript1");
+
+    const bloc2 = document.getElementById("bloc2"); 
+    const descript2 = document.getElementById("descript2");
+
+    const bloc3 = document.getElementById("bloc3"); 
+    const descript3 = document.getElementById("descript3");
+
+    bloc1.addEventListener('click', (e) => {
+        ShowContent(1);
+    });
+
+    bloc2.addEventListener('click', (e) => {
+        ShowContent(2); 
+    })
+
+    bloc3.addEventListener('click', (e) => {
+        ShowContent(3); 
+    })
+
+    function ShowContent(number){
+        returnButton.className = ReturnShow;
+        bloc1.className = "hidden"; 
+        bloc2.className = "hidden"; 
+        bloc3.className = "hidden";
+        descript1.className = "hidden";
+        descript2.className = "hidden";
+        descript3.className = "hidden";
+
+        switch(number){
+            case 1:
+                bloc1.className =Select;
+                descript1.className = "";
+                break;
+            case 2: 
+                bloc2.className = Select; 
+                descript2.className = "";
+                break; 
+            case 3:
+                bloc3.className = Select; 
+                descript3.className = "";
+                break; 
+        }
+    }
+
+    function HideContent(){
+        returnButton.className = ReturnHide; 
+        descript1.className = "hidden";
+        descript2.className = "hidden";
+        descript3.className = "hidden";
+        bloc1.className = Unselect; 
+        bloc2.className = Unselect;
+        bloc3.className = Unselect; 
+    }
+});
+
